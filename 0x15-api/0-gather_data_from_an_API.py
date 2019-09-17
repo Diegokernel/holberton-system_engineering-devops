93665513e3ab2387f27711ca3b45c8e7bf950ae8
Diegokernel <777@holbertonschool.com>
2019-09-16 19:21:04 -0500

    add file 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+" returns information about his/her TODO list progress."
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    complete = []
+    user = get('https://jsonplaceholder.typicode.com/users/{}'
+               .format(argv[1])).json()
+    todo = get('https://jsonplaceholder.typicode.com/todos?userId={}'
+               .format(argv[1])).json()
+    for attr in todo:
+        if attr.get('completed') is True:
+            complete.append(item.get('title'))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user.get('name'), len(complete), len(todo)))
+    for title in complete:
+        print('\t {}'.format(title))
