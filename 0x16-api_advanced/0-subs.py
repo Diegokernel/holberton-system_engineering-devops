1bc836d172e09620d8e3d6be0bcc02966142c1d6
Diegokernel <777@holbertonschool.com>
2019-09-19 21:18:56 -0500

    add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" calls Reddit API and returns the number of subscribers """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ queries the Reddit API and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {"User-Agent": "Diego"}
+    subs = requests.get(url, headers=user_agent, allow_redirects=False)
+    if subs is not None:
+        data = subs.json().get("data")
+        num_subs= data.get("subscribers")
+        return (num_subs)
+    return (0)
